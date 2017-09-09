a0dc12ec96e1a383b190e617c7ee6b558e926c6e
Jeremy Mundell <jeremiahmundell@gmail.com>
2017-09-09 18:59:28 -0400

    js changes

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,2 +1,18 @@
 // alert("I work");
 
+ 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDPaJg9A59VdAuDW6KjuXQNyj-ZwW5YX9s",
+    authDomain: "trainscheduler-478cf.firebaseapp.com",
+    databaseURL: "https://trainscheduler-478cf.firebaseio.com",
+    projectId: "trainscheduler-478cf",
+    storageBucket: "trainscheduler-478cf.appspot.com",
+    messagingSenderId: "35697103270"
+  };
+  firebase.initializeApp(config);
+
+
+//created the variable "database"
+  var database = firebase.database();
+ 
