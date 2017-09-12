914a3af114d487552a6b2dad75efe0062ac0faf5
Jeremy Mundell <jeremiahmundell@gmail.com>
2017-09-12 13:11:30 -0400

    new js

--- assets/script/app.js
+++ assets/script/app.js
@@ -15,4 +15,45 @@
 
 //created the variable "database"
   var database = firebase.database();
+
+  //creating a function for the submit button
+
+  $("#s").on("click", function(){
+
+// here we are creating variables that correspond to the input boxes
+  	var nameInput = $("#nameInput").val(),trim();
+  	var destinationInput = $("#destinationInput").val(),trim();
+  	var timeInput = $("#timeInput").val(),trim();
+  	var freqInput = $("#freqInput").val(),trim();
+
+  	//next we want to combine all the input variables in to a single variable
+
+  	var newTrain = {
+  		name : nameInput,
+  		destination : destinationInput,
+  		time : timeInput,
+  		frequency : freqInput
+  	}
+
+  	// push the input data to Firebase
+
+  	database.ref().push(newTrain);
+
+
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.time);
+  console.log(newTrain.frequency);
+
+
+  //clearing the input from the previous train for the next train input
+
+  $("#nameInput").val("");
+  $("#destinationInput").val("");
+  $("#timeInput").val("");
+  $("#frequencyInput").val("");
+
+
+
+  });
  
